ffd9d71d2d305a7ee5510b3ab993abb1e62b2ab2
ansehun <ansehoon1999@gmail.com>
2025-03-01 14:31:16 +0900

    chore: add annotation to bitmap extension file

--- handwriting-compose/src/commonMain/kotlin/com/henni/handwriting/kmp/extension/Bitmap.kt
+++ handwriting-compose/src/commonMain/kotlin/com/henni/handwriting/kmp/extension/Bitmap.kt
@@ -4,7 +4,13 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.ImageBitmapConfig
 import androidx.compose.ui.unit.IntSize
 
-
+/**
+ * Creates and returns a new `ImageBitmap` with the specified size and ARGB8888 color format.
+ * The `ImageBitmap` is initialized with the given width and height.
+ *
+ * @param size The size (width and height) of the bitmap to create.
+ * @return A new `ImageBitmap` with the specified size and ARGB8888 configuration.
+ */
 internal inline fun getBitmap(size: IntSize): ImageBitmap {
     return ImageBitmap(
         size.width,
